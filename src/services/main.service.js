2a86a2fe0154a47917d4710172e04bae7fc960b0
dominik791 <dominik.broj@gmail.com>
2017-05-28 17:03:32 +0200

    fetch text data from contentful

--- src/services/main.service.js
+++ src/services/main.service.js
@@ -1,8 +1,5 @@
-import data from '../mocks/main'
+import axios from 'axios';
+import config from '../../config.json';
 
 export const fetchMainData = () =>
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(data);
-    }, 500);
-})
+  axios.get(`${config.APIUrl}/spaces/${config.spaceID}/entries/3hi46zzYLYaSg66wgQO6IQ?access_token=${config.deliveryAPIToken}`)
