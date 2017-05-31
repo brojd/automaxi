920d68d61f924bfe34fbba7c70f777222c32af07
dominik791 <dominik.broj@gmail.com>
2017-05-31 23:08:52 +0200

    display logo

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './Header.stylesheet.css';
+import logo from './images/logo.png';
 
 const Header = () =>
   <header className={styles.Header}>
-    header
+    <img
+      src={logo}
+      alt="logo"
+      className={styles.logo}
+    />
   </header>;
 
 export default Header;
