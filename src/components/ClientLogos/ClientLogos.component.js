b68c159313f350f0f1d3fac7ca3c8a275e277bce
dominik791 <dominik.broj@gmail.com>
2017-06-04 21:29:42 +0200

    add animations to ClientLogos

--- src/components/ClientLogos/ClientLogos.component.js
+++ src/components/ClientLogos/ClientLogos.component.js
@@ -2,24 +2,63 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './ClientLogos.stylesheet.css';
 import { headerDict } from '../../static_data/dictionary';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+let currentLogoIndex = -1;
 
 class ClientLogos extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      currentLogoIndex
+    }
+    this.changeImgIndex = this.changeImgIndex.bind(this);
+  }
+
+  changeImgIndex() {
+    const nextIndex = this.state.currentLogoIndex === (this.props.logoList.length - 1) ?
+      0
+      : this.state.currentLogoIndex + 1;
+    currentLogoIndex = nextIndex;
+    this.setState({
+      currentLogoIndex: nextIndex
+    });
+  }
+
+  componentDidMount() {
+    this.changeImgIndex()
+    this.logoInterval = window.setInterval(this.changeImgIndex, 5100);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.logoInterval);
+  }
 
   render() {
     return (
       <section className={styles.ClientLogos}>
         <h4>{headerDict.clientLogosText}</h4>
         <div>
-          {
-            this.props.logoList.map((logo, index) =>
-              <img
-                src={logo}
-                key={index}
-                height={this.props.logoHeight}
-                width={'auto'}
-              />
-            )
-          }
+          <ReactCSSTransitionGroup
+            transitionName={styles}
+            transitionEnterTimeout={5000}
+            transitionLeaveTimeout={1}
+          >
+            {
+              this.props.logoList.map((logo, index) =>
+                currentLogoIndex === index ?
+                  <img
+                    src={logo}
+                    alt='client_logo'
+                    height={this.props.logoHeight}
+                    width={'auto'}
+                    className={styles.logo}
+                  />
+                  : null
+              )
+            }
+          </ReactCSSTransitionGroup>
         </div>
       </section>
     )
