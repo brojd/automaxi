9b24d3aaa31cc754100ed61ed629b46cc526fa19
dominik791 <dominik.broj@gmail.com>
2017-07-02 22:25:18 +0200

    show/hide Header

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,18 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+import cn from 'classnames';
 import Nav from '../Nav/Nav.component';
 import styles from './Header.stylesheet.css';
 import logo from './images/logo.png';
 
-const Header = () =>
-  <header className={styles.Header}>
-    <div className={styles.contentWrapper}>
-      <img
-        src={logo}
-        alt="logo"
-        className={styles.logo}
-      />
-      <Nav />
-    </div>
-  </header>;
+class Header extends Component {
+  constructor() {
+    super();
+    this.onScroll = this.onScroll.bind(this);
+    this.toggleHeader = this.toggleHeader.bind(this);
+    this.state = {
+      scrollY: 0,
+      headerVisible: true
+    }
+  }
+
+  toggleHeader() {
+    this.setState({ headerVisible: !this.state.headerVisible });
+  }
+
+  onScroll() {
+    this.setState({
+      headerVisible: window.scrollY <= this.state.scrollY,
+      scrollY: window.scrollY
+    });
+  }
+
+  componentWillMount() {
+    window.addEventListener('scroll', this.onScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll);
+  }
+
+  render() {
+    return (
+      <header className={cn(
+        styles.Header,
+        { [styles.hideHeader]: !this.state.headerVisible },
+        { [styles.showHeader]: this.state.headerVisible }
+      )}>
+        <div
+          className={cn(
+            styles.arrow,
+            {[styles['arrow--down']]: !this.state.headerVisible},
+            {[styles['arrow--up']]: this.state.headerVisible}
+          )}
+          onClick={this.toggleHeader}
+        >
+          &#8249;
+        </div>
+        <div className={styles.contentWrapper}>
+          <img
+            src={logo}
+            alt="logo"
+            className={styles.logo}
+          />
+          <Nav />
+        </div>
+      </header>
+    )
+  }
+}
+
 
 export default Header;
