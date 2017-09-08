faf9e41ef4b78757bf318261b256789bd487732d
dominik791 <dominik.broj@gmail.com>
2017-09-08 23:16:40 +0200

    change port

--- src/services/main.service.js
+++ src/services/main.service.js
@@ -2,4 +2,4 @@ import axios from 'axios';
 import config from '../../config.json';
 
 export const fetchMainData = () =>
-  axios.get(`http://localhost:8000/spaces/${config.spaceID}/entries/3hi46zzYLYaSg66wgQO6IQ`)
+  axios.get(`http://localhost:8008/spaces/${config.spaceID}/entries/3hi46zzYLYaSg66wgQO6IQ`)
